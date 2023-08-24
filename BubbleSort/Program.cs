b4ccc89019dccadbc68ba4224f1b761ba0ae76b1
mksvf <mksvf@DESKTOP-2KIFKMJ>
2023-08-24 13:16:34 +0300

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> Arr = new List<int>();
+            string inputLine = Console.ReadLine();
+            string[] SepInputLine = inputLine.Split(" ");
+            foreach (string numStr in SepInputLine)
+            {
+                bool isParsed = int.TryParse(numStr, out int num);
+                if (isParsed)
+                {
+                    Arr.Add(num);
+                }
+            }
+            bool flag = false;
+            while (!flag)
+            {
+                flag = true;
+                for (int i = 0; i < Arr.Count - 1; i++)
+                    if (Arr[i] > Arr[i + 1])
+                    {
+                        int c = Arr[i];
+                        Arr[i] = Arr[i + 1];
+                        Arr[i + 1] = c;
+                        flag = false;
+                    }
+            }
+            foreach (int num in Arr)
+            {
+                Console.Write(num);
+            }
+        }
+    }
+}
---
+++ Encrypt-decrypt/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string? inputLine = Console.ReadLine();
+            Crypt crypter = new Crypt();
+            Console.WriteLine("Шифровка:");
+            crypter.EnCrypt(ref inputLine);
+            Console.WriteLine(inputLine);
+            Console.WriteLine("Расшифровка:");
+            crypter.DeCrypt(ref inputLine);
+            Console.WriteLine(inputLine);
+        }
+    }
+    public class Crypt
+    {
+        private string Al = "АБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЪЬЫЭЮЯабвгдеёжзиклмнопрстуфхцчшщъьыэюя";
+        private string res;
+ 
+        public void EnCrypt(ref string inputline)
+            {
+                res = string.Empty;
+                foreach (char ch in inputline)
+                    {
+                        int i = 0;
+                        while (ch != Al[i])
+                        {
+                            i++;
+                        }
+                    if (i == 0)
+                        res += "Ю";
+                    else
+                        if (i == 1)
+                            res += "Я";
+                        else
+                            if (i == 32)
+                                res += "ю";
+                            else
+                                if (i == 33)
+                                    res += "я";
+                                else
+                                    res += Al[i - 2];
+                    }
+                inputline = res;
+
+            }
+        public void DeCrypt(ref string inputline)
+            {
+                res = string.Empty;
+                foreach (char ch in inputline)
+                    {
+                        int i = 0;
+                        while (ch != Al[i])
+                        {
+                            i++;
+                        }
+                        if (i == 31)
+                            res += "А";
+                        else
+                            if (i == 32)
+                                res += "Б";
+                            else
+                                if (i == 62)
+                                    res += "а";
+                                else
+                                    if (i == 63)
+                                        res += "б";
+                                    else
+                                        res += Al[i + 2];
+                    }
+                inputline = res;
+
+        }
+    }
+}
+
+           
